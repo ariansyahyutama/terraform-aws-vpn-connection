ca8691f8b26c1c3aca931a94f312b9dd38c03ae4
Ariansyah Yutama <ariansyahy@yahoo.com>
2022-03-04 07:03:05 +0700

    modify the aws vpn connection for multiple vpn ;

--- main.tf
+++ main.tf
@@ -21,9 +21,11 @@ resource "aws_customer_gateway" "default" {
 
 # https://www.terraform.io/docs/providers/aws/r/vpn_connection.html
 resource "aws_vpn_connection" "default" {
-  count                    = local.enabled ? 1 : 0
-  vpn_gateway_id           = join("", aws_vpn_gateway.default.*.id)
-  customer_gateway_id      = join("", aws_customer_gateway.default.*.id)
+  #count                    = local.enabled ? 1 : 0
+  #vpn_gateway_id           = join("", aws_vpn_gateway.default.*.id)
+  count                    = local.enabled ? length(aws_customer_gateway.default.ids) : 0
+  vpn_gateway_id           = aws_vpn_gateway.default.id #join("", aws_vpn_gateway.default.*.id)
+  customer_gateway_id      = element(aws_customer_gateway.default.ids[*],index)
   type                     = "ipsec.1"
   static_routes_only       = var.vpn_connection_static_routes_only
   local_ipv4_network_cidr  = var.vpn_connection_local_ipv4_network_cidr
